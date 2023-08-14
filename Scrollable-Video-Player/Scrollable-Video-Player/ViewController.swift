b497a1955dd87c6aee9173bd1bd7f838f2c4ba14
Mohammad Sameer <mohammad.sameer@ZL-BLR-MAC574.local>
2023-08-14 12:29:17 +0530

    Added xcode project

---
+++ Scrollable-Video-Player/Scrollable-Video-Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scrollable-Video-Player
+//
+//  Created by Mohammad Sameer on 14/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Scrollable-Video-Player/Scrollable-Video-PlayerUITests/Scrollable_Video_PlayerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Scrollable_Video_PlayerUITestsLaunchTests.swift
+//  Scrollable-Video-PlayerUITests
+//
+//  Created by Mohammad Sameer on 14/08/23.
+//
+
+import XCTest
+
+final class Scrollable_Video_PlayerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
